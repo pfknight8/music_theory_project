795ad60bcd3830db548017f0f8befbf59b144436
Patrick Knight <pfknight2@gmail.com>
2023-02-27 22:38:50 -0500

    edited functionality to chord creator

--- src/components/chordcreater.tsx
+++ src/components/chordcreater.tsx
@@ -11,9 +11,7 @@ const ChordCreator = () => {
   const [chordList, setChordList] = useState<Array<ChordSet>>([])
   const [noteQual, setNoteQual] = useState("natural")
 
-  const MakeTriads = (note: any) => {
-    let indexOfNote = Notes.chromatic_flat.indexOf(note);
-    // let chordObj = {};
+  const MakeTriads = (indexOfNote: any) => {
     let chordArr: any[] = [];
     Notes.chord_semitones.forEach(element => {
       let noteArr: any[] = [];
@@ -21,8 +19,17 @@ const ChordCreator = () => {
         let pushVal = element.steps[i]+indexOfNote;
         noteArr.push(pushVal);
       };
-      chordArr.push({"name": element.name, "steps": noteArr.splice(0)})
-      // chordObj = {...chordObj, [`${element.name}`]: noteArr.splice(0)};
+      let noteArrLtr: any[] = [];
+      for (let k=0; k<noteArr.length; k++) {
+        let noteIndex = noteArr[k]%12
+        if (noteQual === "natural" || noteQual === "flat"){
+          noteArrLtr.push(Notes.chromatic_flat[noteIndex])
+        } else {
+          noteArrLtr.push(Notes.chromatic_sharp[noteIndex])
+        }
+      }
+      chordArr.push({"name": element.name, "steps": noteArrLtr.splice(0)})
+      noteArr = []
     });
     setChordList(chordArr)
   }
@@ -33,7 +40,19 @@ const ChordCreator = () => {
     if (noteSelect.length>1 || (noteSelect.charCodeAt(0) < 65 || noteSelect.charCodeAt(0) > 71)) {
       alert('Not a valid note!')
     }
-    MakeTriads(noteSelect)
+    let noteSearch = ""
+    let indexOfNote: number = 0
+    if (noteQual === "sharp"){
+      noteSearch = `${noteSelect}`+`\u266f`
+      indexOfNote = Notes.chromatic_sharp.indexOf(noteSearch)
+    } else if (noteQual === "flat"){
+      noteSearch = `${noteSelect}`+`\u266d`
+      indexOfNote = Notes.chromatic_flat.indexOf(noteSearch)
+    } else {
+      noteSearch = noteSelect
+      indexOfNote = Notes.chromatic_flat.indexOf(noteSearch)
+    }
+    MakeTriads(indexOfNote)
   }
 
   const handleNoteSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
@@ -66,7 +85,7 @@ const ChordCreator = () => {
             value="sharp"
             onChange={() => setNoteQual("sharp")}
           />
-          <label htmlFor="sharp-opt">Sharp (#)</label>
+          <label htmlFor="sharp-opt">Sharp (&#x266f;)</label>
           <input
             name="shap-flat-select"
             type="radio"
@@ -81,8 +100,8 @@ const ChordCreator = () => {
       <section id="triad-list">
         <h3>Triad Chords</h3>
         <div>{chordList?.map((item, index) => (
-          <div>
-            {item.name}
+          <div key={index}>
+            {item.name} = {item.steps}
           </div>
         ))}</div>
         <p>A note on Inversions (link to page) Any chord can be inverted. This should also include (or lead to) discussing voicing, where notes above the root are moved into higher octaves, or multiplied. There are mumbo-jumbo ways to describe inversions, but to me the simplest is to say that you bring the notes up in a stack; for example in triads, the first inversion brings the root up to the higest note, the second will bring the root and third up (so fifth is lowest). Inversions can be combined with voicing, so even if the root and fifth are both brought up (leaving the third as the lowest note), this would still be a first inversion, with the 5th being voiced. Thus, it may be better defined by which note is being left as the lowest position.</p>
